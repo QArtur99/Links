4fd3ce6bc20829c01829cd128e958c8c64039ad4
ART_F <artur2113@o2.pl>
2019-04-01 00:01:31 +0200

    feat Color Ball privacy policy

--- js/main.js
+++ js/main.js
@@ -17,6 +17,19 @@ let ccListBySa3 = [
     , 'Swirl by Uday',
 ]
 
+let freePicHref = [
+    'https://www.freepik.com/free-photos-vectors/circle'
+    , 'https://www.freepik.com/free-photos-vectors/background'
+    , 'https://www.freepik.com/free-photos-vectors/sport'
+    
+]
+
+let freePicInner = [
+    'Circle vector created by freepik - www.freepik.com'
+    , 'Background vector created by brgfx - www.freepik.com'
+    , 'Sport vector created by sergey_kandakov - www.freepik.com'
+]
+
 const licenceLinkCcBy4 = 'https://creativecommons.org/licenses/by/4.0/'
 const licenceCcBy4 = 'CC BY 4.0'
 const licenceCcBySa3 = 'CC BY-SA 3.0'
@@ -26,6 +39,7 @@ const licenceLinkCcBySa3 = 'https://creativecommons.org/licenses/by/4.0/'
 document.addEventListener('DOMContentLoaded', (event) => {
     fillList(ccListBy4, licenceCcBy4, licenceLinkCcBy4)
     fillList(ccListBySa3, licenceCcBySa3, licenceLinkCcBySa3)
+    fillListFreePic(freePicInner, freePicHref)
 });
 
 fillList = (ccList, licenceString, licenceLink) => {
@@ -33,6 +47,13 @@ fillList = (ccList, licenceString, licenceLink) => {
     ccList.forEach(listItem => {
         ul.append(createListItem(listItem, licenceString, licenceLink))
     })
+}
+
+fillListFreePic = (freePicInner, freePicHref) => {
+    const ul = document.getElementById('freepik-list')
+    for(let i=0; freePicInner.length > i; i++){
+        ul.append(createListItemFreePic(freePicInner[i], freePicHref[i]))
+    }
 }
 
 
@@ -45,6 +66,20 @@ createListItem = (listItem, licenceString, licenceLink) => {
     const licence = document.createElement('a')
     licence.innerHTML = licenceString
     licence.href = licenceLink
+    text.append(licence)
+
+    li.append(text)
+
+    return li
+}
+
+createListItemFreePic = (freePicInner, freePicHref) => {
+    const li = document.createElement('li')
+    const text = document.createElement('p')
+    
+    const licence = document.createElement('a')
+    licence.innerHTML = freePicInner
+    licence.href = freePicHref
     text.append(licence)
 
     li.append(text)
